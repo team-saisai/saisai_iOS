301867055b027fcce8acde0385102d2031cbc6aa
duthd3 <duthd3@naver.com>
2025-07-07 22:09:55 +0900

    feat: 디자인 시스템 베이스 구현, 홈화면 UI 구현

---
+++ SaiSai/Extension/Fonts+.swift
@@ -0,0 +1,46 @@
+//
+//  Fonts+.swift
+//  SaiSai
+//
+//  Created by yeosong on 7/5/25.
+//
+import SwiftUI
+
+extension Font {
+    // hakgoyansimSamulham 폰트 한종류
+    static let hakgoyansimSamulham: Font = .custom("OTHakgyoansimSamulhamR", size: 22)
+    
+    enum Pretendard {
+        case light
+        case regular
+        case medium
+        case semibold
+        case bold
+        case extrabold
+        case heavy
+        
+        var name: String {
+            switch self {
+            case .light:
+                return "Pretendard-Light"
+            case .regular:
+                return "Pretendard-Regular"
+            case .medium:
+                return "Pretendard-Medium"
+            case .semibold:
+                return "Pretendard-Semibold"
+            case .bold:
+                return "Pretendard-Bold"
+            case .extrabold:
+                return "Pretendard-ExtraBold"
+            case .heavy:
+                return "Pretendard-Heavy"
+            }
+        }
+    }
+    // pretendard폰트 기본체는 medium
+    // 사용법 - .font(.pretendard(.medium, 12))
+    static func pretendard(_ weight: Pretendard = .medium, size: CGFloat) -> Font {
+        return custom(weight.name, size: size)
+    }
+}
---
+++ SaiSai/SaiSaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaiSaiApp.swift
+//  SaiSai
+//
+//  Created by yeosong on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SaiSaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SaiSaiHomeView()
+        }
+    }
+}
