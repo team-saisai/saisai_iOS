598cc12972d08d6992f88377fe84e184c638e727
ch <ckdgus1120@gmail.com>
2025-07-19 19:32:45 +0900

    fix: 코스 리스트 스크롤 영역 변경

--- SaiSai/Feature/Course/View/CourseView.swift
+++ SaiSai/Feature/Course/View/CourseView.swift
@@ -12,50 +12,53 @@ struct CourseView: View {
     @StateObject var vm: CourseViewModel = .init()
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            LazyVStack(spacing: 0) {
-                HStack {
-                    Text("코스")
-                        .font(.pretendard(.semibold, size: 20))
-                        .foregroundStyle(.white)
-                    Spacer()
-                }
-                .padding(.bottom, 17)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack(spacing: 8) {
-                        ChallengeFilter()
-                        // TODO: - ForEach(theme) 추가 예정
-                        
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마1", font: .pretendard(size: 14), action: {})
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마2", font: .pretendard(size: 14), action: {})
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마3", font: .pretendard(size: 14), action: {})
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마4", font: .pretendard(size: 14), action: {})
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마5", font: .pretendard(size: 14), action: {})
-                        RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마6", font: .pretendard(size: 14), action: {})
-                    }
-                    .padding(.bottom, 24)
-                    .frame(maxWidth: .infinity)
-                }
-                
-                if !vm.contentInfoList.isEmpty {
-                    ForEach(vm.contentInfoList.indices, id: \.self) { index in
-                        HStack {
-                            SingleHorizontalCourseView(contentInfo: vm.contentInfoList[index])
-                        }
-                        .padding(.bottom, 20)
-                    }
-                }
-                
-                if vm.hasReachedLast {
-                    ProgressView()
-                        .onAppear() {
-                            vm.fetchData()
-                        }
-                }
+        VStack(spacing: 0) {
+            HStack {
+                Text("코스")
+                    .font(.pretendard(.semibold, size: 20))
+                    .foregroundStyle(.white)
+                Spacer()
             }
-            .padding(.top, 22)
-            .padding(.horizontal, 20)
+            .padding(.vertical, 18)
+            .padding(.leading, 24)
+            
+            ScrollView(.vertical, showsIndicators: true) {
+                LazyVStack(spacing: 0) {
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 8) {
+                            ChallengeFilter()
+                            // TODO: - ForEach(theme) 추가 예정
+                            
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마1", font: .pretendard(size: 14), action: {})
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마2", font: .pretendard(size: 14), action: {})
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마3", font: .pretendard(size: 14), action: {})
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마4", font: .pretendard(size: 14), action: {})
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마5", font: .pretendard(size: 14), action: {})
+                            RoundedButton(radius: 6, bgColor: .gray80, horizontalPadding: 14, verticalPadding: 8.5, text: "테마6", font: .pretendard(size: 14), action: {})
+                        }
+                        .padding(.bottom, 24)
+                        .frame(maxWidth: .infinity)
+                    }
+                    
+                    if !vm.contentInfoList.isEmpty {
+                        ForEach(vm.contentInfoList.indices, id: \.self) { index in
+                            HStack {
+                                SingleHorizontalCourseView(contentInfo: vm.contentInfoList[index])
+                            }
+                            .padding(.bottom, 20)
+                        }
+                    }
+                    
+                    if vm.hasReachedLast {
+                        ProgressView()
+                            .onAppear() {
+                                vm.fetchData()
+                            }
+                    }
+                }
+                .padding(.horizontal, 20)
+            }
         }
         .background(.gray90)
     }
