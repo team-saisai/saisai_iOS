4f28b7eccb245940d754ff2c1c1f0b98e74f8cdc
ch <ckdgus1120@gmail.com>
2025-07-27 01:24:35 +0900

    fix: 뱃지 컬렉션 subtitle 제거

--- SaiSai/Feature/Home/View/BadgeCollectionView.swift
+++ SaiSai/Feature/Home/View/BadgeCollectionView.swift
@@ -38,12 +38,6 @@ extension BadgeCollectionView {
     
     private func BadgesContainerView(_ badges: [BadgeInfo]) -> some View {
         VStack(alignment: .leading, spacing: 14) {
-            HStack {
-                Text("Subtitle")
-                    .font(.pretendard(.medium, size: 16))
-                    .foregroundStyle(.white)
-                Spacer()
-            }
             
             if badges.count == 8 {
                 HStack {
